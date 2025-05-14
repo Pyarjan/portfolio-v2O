d2df430e13eb6f31c6d4db1dca6eb196f7ea2606
Pyarjan Thapa <pyarjanthapa12@gmail.com>
2025-05-14 16:25:24 +0545

    remove node_modules

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/portfolio-v2O/', 
+  base: 'portfolio-v2O', 
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
